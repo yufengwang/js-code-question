d13344b8f3470c69f33fbfecaeacbe50a0ef7b61
richard <yufengwang1024@gmail.com>
2023-02-01 17:27:45 +0800

    feat: update

--- promise-concurrency.js
+++ promise-concurrency.js
@@ -1,40 +1,41 @@
 /*
  * 实现 Scheduler 中的 add 方法，满足:
 
-  add 方法只有一个入参 task ， 类型为 () => Promise<any>
-  按 add 调用顺序执行任务
-  同一时刻只能存在至多2个并发任务
-  可自由添加实例属性、方法
+ 1. add 方法只有一个入参 task ， 类型为 () => Promise<any>
+ 2. 按 add 调用顺序执行任务
+ 3. 同一时刻只能存在至多2个并发任务
+ 4. 可自由添加实例属性、方法
 
 const timeout = (time) => new Promise(resolve => {
   setTimeout(resolve, time)
 })
 
 const scheduler = new Scheduler()
+
 const addTask = (time, order) => {
   scheduler.add(() => timeout(time))
     .then(() => console.log(order))
 }
 
 限制同一时刻只能执行2个task
+
 addTask(4000, '1')
 addTask(3500, '2')
 addTask(4000, '3')
 addTask(3000, '4')
 
-Scheduler ？
-4 秒后打印1
-3.5 秒打印2
-3 进入队列，到7.5秒打印3
-...
+打印顺序 2,1,4,3
 */
 
 // 实现
 
 class Scheduler {
   constructor() {
+    // 排队
     this.queue = [];
+    // 处理中
     this.processing = [];
+    // 并发任务数上限
     this.limit = 2;
   }
 
@@ -47,4 +48,5 @@ class Scheduler {
       fn().then(() => {});
     });
   }
+  run() {}
 }
